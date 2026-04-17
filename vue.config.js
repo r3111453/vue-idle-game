910f55fbd53b43ab8e70c00d21624fa1bae7d055
小樹苗工作室 <92939444+r3111453@users.noreply.github.com>
2026-04-18 01:28:43 +0800

    Add files via upload

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,16 @@
 const CompressionPlugin = require("compression-webpack-plugin")
 module.exports = {
-  // 选项...
-  publicPath: '',
+  // 使用相对路径，兼容子目录和根目录
+  publicPath: './',
   productionSourceMap: false,
   configureWebpack: config => {
     if (process.env.NODE_ENV === 'production') {
       return {
         plugins: [
           new CompressionPlugin({
-            test: /\.js$|\.html$|.\css/, //匹配文件名
-            threshold: 1024, //对超过10k的数据压缩
-            deleteOriginalAssets: false //是否删除源文件
+            test: /\.js$|\.html$|.\css/,
+            threshold: 1024,
+            deleteOriginalAssets: false
           })
         ]
       }
