1a1756e82c0080c78f2bdb10592575d0919f35db
小樹苗工作室 <92939444+r3111453@users.noreply.github.com>
2026-04-20 20:41:46 +0800

    Update index.vue

--- src/views/index.vue
+++ src/views/index.vue
@@ -30,7 +30,7 @@
         </template>
         <template v-slot:tip>
           <p class="info">* 当前生命值/最大生命值</p>
-          <p class="info">* 每秒会回复2%的最大生命值</p>
+          <p class="info">* 每秒会回复3%的最大生命值</p>
           <p class="info">最大生命值=200+({{attribute.MAXHP.info[1]}}+{{attribute.MAXHP.info[0]}})*{{100+attribute.MAXHP.info[2]}}%</p>
         </template>
 
